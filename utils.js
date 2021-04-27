e6ab0ed85e73e33dfff3a5081c29988f3dc03e4a
Step Molotov <stepmolotov@users.noreply.github.com>
2021-04-27 15:59:07 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -38,53 +38,16 @@ function getDistance(p0, p1){
 	return Math.sqrt(dx * dx + dy * dy);
 }
 
-
-function findEquation(p0, p1){
-	var dy = p1.y - p0.y,
-			dx = p1.x - p0.x;
-	if(dx != 0){
-		m = dy/dx;
-		// y = mx+q -> q = y-mx
-		q = p0.y - m*p0.x;
-	}else{ // deal with the vertical line
-		return null;
-	}
-	return {
-		m: m,
-		q: q
-	};
-}
-
-
-function getLineValue(p0, p1, x){
-	var eq = findEquation(p0, p1);
-	if(!eq)
-		return null
-	else // y = mx+q
-		return eq.m * x + eq.q; 
-}
-
-
-// TODO: what about other line types? Left/Right for instance
-function aboveSegment(s0, s1, p){
-	// return 1 if the point P is above the line, 0 if on it, -1 otherwise
-	var line = findEquation(s0, s1);
-	var liney = line.m*p.x + line.q;
-	if (p.y > liney) // above
-		return 1;
-	else if(p.y == liney) // on
-		return 0;
-	else // below
-		return -1;
-}
-
 /* ---------- // ---------- */
 
 
 
 
 /* ---------- Intersections ---------- */
-function segmentIntersect(p0, p1, p2, p3) {
+function segmentIntersect(p0, p1, p2, p3){
+	// A1x + B1y = C1	| A2x + B2y = C2
+	// if B==0 -> vertical line
+	// if A==0 -> horizontal line
 	var A1 = p1.y - p0.y,
 		B1 = p0.x - p1.x,
 		C1 = A1 * p0.x + B1 * p0.y,
@@ -93,10 +56,21 @@ function segmentIntersect(p0, p1, p2, p3) {
 		C2 = A2 * p2.x + B2 * p2.y,
 		denominator = A1 * B2 - A2 * B1;
 
-	if(denominator == 0) {
+	if(denominator == 0){
+		// not vertical lines
+		if(B1 != 0 && B2 != 0){
+			// y intercept (q) is the same -> collinear
+			if((-C1/B1) == (-C2/B2)){
+				//console.log('collinear');
+			// y intercept (q) is different -> parallel
+			}else{
+				//console.log('parallel');
+			}
+		// not horizontal lines
+		}else if(A1 != 0 && A2 != 0){
+			return null;
+		}
 		return null;
-		// y intercept is different -> parallel
-		// y intercept is different -> collinear
 	}
 
 	var intersectX = (B2 * C1 - B1 * C2) / denominator;
@@ -132,34 +106,6 @@ function segmentIntersect(p0, p1, p2, p3) {
 }
 
 
-function segmentPolyIntersect(s0, s1, poly) {
-	intersected = false;
-	intersections = [];
-	counter = poly.points.length;
-
-	for(var i = 0; i < poly.points.length; i++) {
-		var p0 = poly.points[i];
-		var p1 = poly.points[(i + 1) % poly.points.length];
-		var intersect = segmentIntersect(s0, s1, p0, p1);
-		if(intersect){
-			intersected = true;
-			//console.log(intersect.type);
-			intersections.push({
-				x: intersect.x,
-				y: intersect.y,
-				id: counter,
-				type: intersect.type,
-				from: i,
-				to: (i + 1) % poly.points.length
-			});
-			counter++;
-		}
-	}
-	if(intersected)
-		return intersections;
-	else
-		return null;
-}
 /* ---------- // ---------- */
 
 
